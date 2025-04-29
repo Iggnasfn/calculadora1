6c445891abf8a00fd3526acc54eb7036b6fc0970
Luis <seivaneluis@gmail.com>
2025-04-28 21:39:47 -0300

    Programa para la suma de dos numeros con el uso de una función.

---
+++ suma.py
@@ -0,0 +1,14 @@
+# calculadora.py
+
+def sumar(a,b):
+
+    return a + b
+
+# Código principal.
+
+if __name__ == "__main__":
+    num1 = int(input("Ingrese primer número: "))
+
+    num2 = int(input("Ingrese segundo número: "))
+
+    print("Resultado:", sumar(num1, num2))
