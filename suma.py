6d1c09fd470b5f415faf79b60f3e081fd9f73c78
Luis <seivaneluis@gmail.com>
2025-05-05 19:34:59 -0300

    Se agrega la calduladora con la función de dividir y sumar.

--- suma.py
+++ suma.py
@@ -1,14 +1,7 @@
 # calculadora.py
 
+# Función suma.
+
 def sumar(a,b):
 
     return a + b
-
-# Código principal.
-
-if __name__ == "__main__":
-    num1 = int(input("Ingrese primer número: "))
-
-    num2 = int(input("Ingrese segundo número: "))
-
-    print("Resultado:", sumar(num1, num2))
